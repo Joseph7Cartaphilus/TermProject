a992cab288d68f7476b9cdd760c1dee8f40efa59
Joseph <artem.harov.120@mail.ru>
2023-02-26 16:54:35 +0300

    + users admin panel view

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    readonly_fields = ('date_joined', 'last_login', 'is_active', 'get_image')
+    list_display_links = ('email', 'username')
+    list_display = (
+        'email', 'username', 'get_image')
+    fieldsets = (
+        ('Info', {
+            'fields': (
+                'username', 'first_name',
+                'last_name', 'email',
+            )
+        }),
+        ('Login', {
+            'fields': ('date_joined', 'last_login', 'is_active'),
+        }),
+        ('Staff', {
+            'fields': ('is_superuser', 'is_staff'),
+        }),
+        ('User password', {
+            'classes': ('collapse',),
+            'fields': ('password',),
+        }),
+        ('Image', {
+            'fields': ('image', 'get_image')
+        }),
+    )
+    add_fieldsets = (
+        (
+            'Info',
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'username', 'first_name',
+                    'last_name', 'email',
+                ),
+            },
+        ),
+        ('Password', {
+            'fields': ('password1', 'password2')
+        }),
+        ('Image', {
+            'fields': ('image',)
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Avatar"
