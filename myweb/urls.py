682060c6c9a136e9098cb70d9e61a2fc23f3acd1
Joseph <artem.harov.120@mail.ru>
2023-02-26 23:04:38 +0300

    + path for main page

--- myweb/urls.py
+++ myweb/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from users import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),  # Главная страница (Parallax Forest)
     path('', include('social_django.urls', namespace='social')),
     path('users/', include('users.urls')),
 ]
