8315102ad087d11b8bbd733a727d8fb9257d5af8
Joseph <artem.harov.120@mail.ru>
2023-02-26 14:49:39 +0300

    + urls for social django

--- users/urls.py
+++ users/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
     path('profile/', views.profile, name='profile')
 ]
