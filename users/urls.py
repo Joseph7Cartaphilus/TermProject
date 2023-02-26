d96f1fccbe523bf17225cb8a1ed29ec3f7ebb546
Joseph <artem.harov.120@mail.ru>
2023-02-26 23:10:30 +0300

    = edit path logout

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),  # Страница аутентификации
     path('register/', views.register, name='register'),  # Страница регистрации
-    path('logout/', views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
-    # Страница аутентификации (выход из системы)
+    path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
+    # Главная страница (Parallax Forest)
     path('profile/', views.profile, name='profile')  # Страница профиля пользователя
 ]
