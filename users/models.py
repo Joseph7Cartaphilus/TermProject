87a5b2cfe2725c31d8e9f7cf397710c63d581618
Joseph <artem.harov.120@mail.ru>
2023-02-26 12:55:12 +0300

    + model user

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Пин/Картинка')
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='users',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions '
+                  'granted to each of their groups.'
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='users',
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
