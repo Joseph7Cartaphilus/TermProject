64bfccbbf403a1187db4a3ecd3a3e7020ca5866e
Joseph <artem.harov.120@mail.ru>
2023-03-08 13:16:05 +0300

    = changed verbose names

--- src/pins/models.py
+++ src/pins/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
-from src.users.models import User
+from users.models import User
 
 
 class BaseModel(models.Model):
@@ -16,8 +16,8 @@ class BaseModel(models.Model):
 
 class PinCategory(BaseModel):
     """Модель категория пинов"""
-    name = models.CharField(max_length=64, unique=True, verbose_name='cat_name')
-    description = models.TextField(null=True, blank=True, verbose_name='cat_desc')
+    name = models.CharField(max_length=64, unique=True, verbose_name='Pin Category name')
+    description = models.TextField(null=True, blank=True, verbose_name='Pin Category description')
 
     def __str__(self):
         """Представление объекта"""
@@ -26,12 +26,12 @@ class PinCategory(BaseModel):
 
 class Pin(BaseModel):
     """Модель пина"""
-    title = models.CharField(max_length=30, null=False, blank=False, verbose_name='pin_name')
-    text = models.TextField(max_length=90, null=True, blank=True, verbose_name='pin_desc')
-    img = models.ImageField(upload_to='images/', null=False, blank=False, verbose_name='pin_img')
-    slug = models.SlugField(default='', db_index=True, verbose_name='pin_slug')
-    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT, blank=True, null=True, verbose_name='pin_cat')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='pin_user')
+    title = models.CharField(max_length=30, null=False, blank=False, verbose_name='Pin name')
+    text = models.TextField(max_length=90, null=True, blank=True, verbose_name='Pin description')
+    img = models.ImageField(upload_to='images/', null=False, blank=False, verbose_name='Pin image')
+    slug = models.SlugField(default='', db_index=True, verbose_name='Pin slug')
+    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT, blank=True, null=True, verbose_name='Pin Category')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Pin user')
 
     def __str__(self):
         """Представление объекта"""
