8b68ad4611f8892ab8991acee878c0fbbf867dc8
Joseph <artem.harov.120@mail.ru>
2023-03-10 00:08:28 +0300

    + view\path for add pin

--- src/pins/views.py
+++ src/pins/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpRequest
 
 from pins.models import Pin, PinCategory
+from pins.forms import PinForm
 
 
 def gallery(request: HttpRequest, category_id=None) -> HttpResponse:
@@ -15,3 +16,16 @@ def gallery(request: HttpRequest, category_id=None) -> HttpResponse:
     else:
         context.update({'pins': Pin.objects.all()})
     return render(request, 'gallery.html', context)
+
+
+def add_pin(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = PinForm(request.POST, request.FILES)
+        if form.is_valid():
+            pin = form.save(commit=False)
+            pin.user = request.user
+            pin.save()
+            return redirect('gallery')
+    else:
+        form = PinForm()
+    return render(request, 'add_pin.html', {'form': form})
