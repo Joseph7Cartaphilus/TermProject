96af516c287a1446dd97ce6067842d352588a7db
Joseph <artem.harov.120@mail.ru>
2023-03-13 22:13:03 +0300

    + view for reg\log

--- src/users/views.py
+++ src/users/views.py
@@ -7,36 +7,35 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
-def login(request: HttpRequest) -> HttpResponse:
-    """Функция для аутентификации пользователя"""
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
+def login_or_register(request: HttpRequest) -> HttpResponse:
+    """Функция аутентификации и регистрации пользователя"""
+    login_form = UserLoginForm(prefix='login')
+    register_form = UserRegistrationForm(prefix='register')
+
+    if 'login-username' in request.POST:
+        # Обработка формы аутентификации
+        form = UserLoginForm(data=request.POST, prefix='login')
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
             user = auth.authenticate(username=username, password=password)
             if user and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('profile'))
-    else:
-        form = UserLoginForm()
-    return render(request, 'login.html', {
-        'form': form
-    })
-
-
-def register(request: HttpRequest) -> HttpResponse:
-    """Функция для регистрации пользователя"""
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
+    elif 'register-email' in request.POST:
+        # Обработка формы регистрации
+        form = UserRegistrationForm(data=request.POST, prefix='register')
         if form.is_valid():
+            user = form.save(commit=False)
+            username = user.email.split('@')[0]
+            user.username = username
+            user.save()
             form.save()
             messages.success(request, 'You have successfully registered')
-            return HttpResponseRedirect(reverse('login'))
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'register.html', {
-        'form': form
+            return HttpResponseRedirect(reverse('login_or_register'))
+    return render(request, 'forest.html', {
+        'login_form': login_form,
+        'register_form': register_form
     })
 
 
