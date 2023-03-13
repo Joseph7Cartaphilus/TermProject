0f1fa036e4e96299c7458ae445ab974aa379bc43
Joseph <artem.harov.120@mail.ru>
2023-03-13 22:12:51 +0300

    = changed path to one

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),  # Страница аутентификации
-    path('register/', views.register, name='register'),  # Страница регистрации
+    path('login-or-register/', views.login_or_register, name='login_or_register'),  # Страница регистрации \ авторизации
     path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
     # Главная страница (Parallax Forest)
     path('profile/', views.profile, name='profile'),  # Страница профиля пользователя
+
 ]
