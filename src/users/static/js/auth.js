91af00300bc26828aa6d75045d1fff427a73177a
Joseph <artem.harov.120@mail.ru>
2023-03-17 21:25:44 +0300

    + static for music profile

--- src/users/static/js/auth.js
+++ src/users/static/js/auth.js
@@ -20,3 +20,20 @@
         $("body").toggleClass("sb-sidenav-toggled");
     });
 })(jQuery);
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
+
