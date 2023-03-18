75203e39a29b3180a53f6fbdd79f4811c393f169
Joseph <artem.harov.120@mail.ru>
2023-03-18 19:40:14 +0300

    + models for music app

--- src/music/models.py
+++ src/music/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class BaseModel(models.Model):
+    """Общая модель"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:  # Абстрактная модель
+        abstract = True
+
+
+class Artist(models.Model):
+    """Модель исполнителя"""
+    name = models.CharField(max_length=255, verbose_name='Artist name')
+
+    def __str__(self):
+        """Представление объекта"""
+        return self.name
+
+
+class Track(models.Model):
+    """Модель трека"""
+    title = models.CharField(max_length=255, verbose_name='Track title')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, verbose_name='Track artist')
+    audio_file = models.FileField(upload_to='tracks/', null=False, blank=False, verbose_name='Track audio')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Track user')
+
+    def __str__(self):
+        """Представление объекта"""
+        return self.title
+
+
+class Playlist(models.Model):
+    """Модель плейлиста"""
+    title = models.CharField(max_length=255, verbose_name='Playlist title')
+    tracks = models.ManyToManyField(Track, verbose_name='Playlist tracks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Playlist user')
+
+    def __str__(self):
+        """Представление объекта"""
+        return self.title
