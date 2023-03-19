b35ae395d0b9839372cda6c6900ed9bfc02ec345
Joseph <artem.harov.120@mail.ru>
2023-03-19 10:59:30 +0300

    + notice

--- src/music/apps.py
+++ src/music/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class MusicConfig(AppConfig):
+    """Класс приложения 'music' """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'music'
