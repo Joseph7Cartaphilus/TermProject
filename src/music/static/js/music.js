c2bd271212b55c301e7e94b24938701d5ad8f9db
Joseph <artem.harov.120@mail.ru>
2023-03-24 18:38:33 +0300

    = music player

--- src/music/static/js/music.js
+++ src/music/static/js/music.js
@@ -1,21 +1,34 @@
 let soundButtons = document.querySelectorAll('.soundbutton'),
     audioElements = document.querySelectorAll('.audio'),
-    subheadings = document.querySelectorAll('h5'),
-    headings = document.querySelectorAll('h4');
+    headings = document.querySelectorAll('h4'),
+    subheadings = document.querySelectorAll('h5');
 
 let currentTrack = null;
 
 soundButtons.forEach((soundButton, index) => {
     soundButton.addEventListener('click', e => {
-        soundButton.classList.toggle('paused')
+        // pause current track if playing
+        if (currentTrack && !currentTrack.paused) {
+            currentTrack.pause();
+            headings[currentTrack.index].textContent = "Music";
+            subheadings[currentTrack.index].textContent = "Music";
+            soundButtons[currentTrack.index].classList.add('paused');
+        }
+
+        // play selected track
         if (audioElements[index].paused) {
             audioElements[index].play();
             headings[index].textContent = "Playing";
             subheadings[index].textContent = "Playing";
+            soundButton.classList.remove('paused');
+            currentTrack = audioElements[index];
+            currentTrack.index = index;
         } else {
             audioElements[index].pause();
-            subheadings[index].textContent = "Music";
             headings[index].textContent = "Music";
+            subheadings[index].textContent = "Music";
+            soundButton.classList.add('paused');
+            currentTrack = null;
         }
     })
 });
