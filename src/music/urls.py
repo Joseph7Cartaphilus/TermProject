9c01153a31db24957f0e668efcea46dd821a849d
Joseph <artem.harov.120@mail.ru>
2023-03-25 16:53:05 +0300

    + artist filter

--- src/music/urls.py
+++ src/music/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.player, name='player')
+    path('', views.player, name='player'),
+    path('<int:artist_id>/music/', views.player, name='artist'),
 ]
--- src/music/views.py
+++ src/music/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.decorators import login_required
 
-from music.models import Track
+from music.models import Track, Artist
 
 
-def player(request):
+@login_required
+def player(request: HttpRequest, artist_id=None) -> HttpResponse:
+    """Функция для отображения всех треков | артистов"""
     context = {
         'tracks': Track.objects.all(),
+        'artists': Artist.objects.all(),
     }
+    if artist_id:
+        context.update({'artists': Artist.objects.filter(id=artist_id)})
+    else:
+        context.update({'artists': Artist.objects.all()})
     return render(request, 'player.html', context)
