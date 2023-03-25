387b9b2e79c1e939ad5f92f5fd8b0c694e4185c2
Joseph <artem.harov.120@mail.ru>
2023-03-25 17:11:11 +0300

    + search filed

--- src/music/views.py
+++ src/music/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.decorators import login_required
@@ -8,9 +9,15 @@ from music.models import Track, Artist
 @login_required
 def player(request: HttpRequest, artist_id=None) -> HttpResponse:
     """Функция для отображения всех треков | артистов"""
+    query = request.GET.get('q')
+    if query:
+        tracks = Track.objects.filter(Q(title__icontains=query) | Q(artist__name__icontains=query))
+    else:
+        tracks = Track.objects.all()
     context = {
-        'tracks': Track.objects.all(),
+        'tracks': tracks,
         'artists': Artist.objects.all(),
+        'query': query,
     }
     if artist_id:
         context.update({'artists': Artist.objects.filter(id=artist_id)})
