69b425649539cbb5bf00f2029c387943cd2573f8
Joseph <artem.harov.120@mail.ru>
2023-03-26 14:53:12 +0300

    + description

--- src/music/admin/__init__.py
+++ src/music/admin/__init__.py
@@ -5,6 +5,6 @@ from music.admin.artist import ArtistAdmin
 from music.admin.playlist import PlaylistAdmin
 from music.models import Track, Artist, Playlist
 
-admin.site.register(Track, TrackAdmin)
-admin.site.register(Artist, ArtistAdmin)
-admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(Track, TrackAdmin)  # Регистрация модели Track в админке
+admin.site.register(Artist, ArtistAdmin)  # Регистрация модели Artist в админке
+admin.site.register(Playlist, PlaylistAdmin)  # Регистрация модели Playlist в админке
