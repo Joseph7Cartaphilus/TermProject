47d2835bdacb7c7c817e2163e429213d7893ed49
Joseph <artem.harov.120@mail.ru>
2023-03-26 13:16:44 +0300

    = path

--- src/music/urls.py
+++ src/music/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.player, name='player'),
-    path('<int:artist_id>/music/', views.player, name='artist'),
+    path('<int:artist_id>/', views.player, name='artist'),
     path('playlists/', views.playlists, name='playlists'),
     path('playlist/<int:playlist_id>/', views.playlist, name='playlist'),
 ]
