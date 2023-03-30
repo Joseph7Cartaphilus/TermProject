0caef9a5ef61bb51727582335dd36f5d8dd487d5
Joseph <artem.harov.120@mail.ru>
2023-03-30 21:06:58 +0300

    + artist

--- src/pins/admin/pin.py
+++ src/pins/admin/pin.py
@@ -16,8 +16,8 @@ class PinResource(resources.ModelResource):
 
 class PinAdmin(ImportExportModelAdmin):
     """Отображение пинов в админ панели"""
-    fields = ['img', 'category', 'user']
-    list_display = ['get_image', 'category', 'user']
+    fields = ['img', 'artist', 'user']
+    list_display = ['get_image', 'artist', 'user']
     ordering = ['-created_at']
     list_per_page = 20
     resource_classes = [PinResource]
