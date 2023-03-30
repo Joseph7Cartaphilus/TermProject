b2238aaae32536b56202f9bb13a58bfd6f115c83
Joseph <artem.harov.120@mail.ru>
2023-03-30 21:22:42 +0300

    = place 1 cat

--- src/pins/urls.py
+++ src/pins/urls.py
@@ -7,5 +7,5 @@ from pins import views
 urlpatterns = [
     path('gallery/', views.gallery, name='gallery'),  # Галерея пинов
     path('add_pin/', views.add_pin, name='add_pin'),  # Добавление пинов
-    path('<int:category_id>/gallery/', views.gallery, name='category'),  # Категории пинов
+    path('gallery/<int:category_id>', views.gallery, name='category'),  # Категории пинов
 ]
