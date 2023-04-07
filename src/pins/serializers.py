61a6aa12c97d3270a597dba4c61866bfb4607ae6
Joseph <artem.harov.120@mail.ru>
2023-04-07 17:24:46 +0300

    + serializers

--- src/pins/serializers.py
+++ src/pins/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Pin
+from pins.models import Pin, PinCategory
 
 
 class PinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pin
         fields = ('id', 'img', 'category', 'user')
+
+
+class PinCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PinCategory
+        fields = ('id', 'name')
