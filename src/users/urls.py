ebaab98453c0918f956a062513df8a8f6f75dfc1
Joseph <artem.harov.120@mail.ru>
2023-04-14 15:39:52 +0300

    + web socket

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,11 +3,16 @@ from django.conf import settings
 from django.contrib.auth.views import LogoutView
 
 from . import views
-
+from . import consumers
 urlpatterns = [
     path("login-or-register/", views.login_or_register, name="login_or_register"),  # Страница регистрации \ авторизации
     path("logout/", LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name="logout"),
     # Главная страница (Parallax Forest)
     path("profile/", views.profile, name="profile"),  # Страница профиля пользователя
     path("auth/", views.auth_view, name="auth"),
+]
+
+
+websocket_urlpatterns = [
+    path("ws/my_endpoint/", consumers.MyConsumer.as_asgi()),
 ]
